637227e986db2911f8dd72a894e7b5e698345350
Şafak Yılmaz <68486747+safakyilmaz-mis@users.noreply.github.com>
2023-07-22 23:48:20 +0200

    Create FileSizer.py
    This project will show the dimensions and names of all files in the folder where you put this file

---
+++ FileSizer.py
@@ -0,0 +1,23 @@
+import os
+from os import path
+import shutil
+
+mainPath = path.realpath("fileReader.py")
+os.mkdir("results")
+textFile = open("results.txt","w+")
+size = 0
+
+head,tail = mainPath.split("fileReader.py")
+listFolders = os.listdir(head)
+textFile.write("Files list:\n--------------------------\n")
+for i in range(len(listFolders)):
+    textFile.write(listFolders[i])
+    size += path.getsize(listFolders[i])
+    textFile.write("\n")
+
+textFile.write("\n--------------------------")
+textFile.write("\nFiles size: ")
+textFile.write(str(boyut)+" byte")
+
+textFile.close()
+shutil.move("results.txt",".\\results")
