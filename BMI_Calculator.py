e4ed1d422e4b31ba65cfb840b6d35053d0cf8d0b
Şafak Yılmaz <68486747+safakyilmaz-mis@users.noreply.github.com>
2023-09-06 18:43:55 +0200

    Update BMI_Calculator.py
    V2.0 adding new comments

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,15 +1,29 @@
 #   This program will help you to calculate your BMI
 
 H = float(input("Enter your height (enter meter e.g. 1.75): "))
-#   getting information about height
+#   Getting information about height
 
-W = int(input("Enter your weight: "))
-#   getting information about weight
+W = float(input("Enter your weight: "))
+#   Getting information about weight
 
-Total = round(W/(H*H),2)
+#Total = round(W/(H*H),2)
+Total = float(f"{W/(H*H):.2f}")
 #   calculation
-#   we want to limit our result 2 digits after int number
+#   We want to limit our result to 2 digits after int number
 
-print("Your BMI is: "+str(Total))
-#   we are converting our float to string because, 
-#   we can't print float and str at the same time
+print(f"Your BMI is: {Total}")
+#   We are converting our float to string because, 
+#   We can't print float and str at the same time
+
+if Total < 18.5:
+  print("You have underwaight")
+elif Total < 25:
+    print("You have normal weight")
+elif Total < 30:
+    print("You have overweight")
+elif Total < 35:
+    print("You have obese")
+elif Total > 35:
+    print("You are clinically obese")
+    
+#V2.0 adding new comments
