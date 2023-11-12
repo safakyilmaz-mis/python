87384390c1453781ac39078f52686aefed718853
Şafak Yılmaz <68486747+safakyilmaz-mis@users.noreply.github.com>
2023-11-12 17:26:39 +0100

    Blackjack.py
    This code will help you to create a BlackJack game, have a nice playing!

--- Blackjack.py
+++ Blackjack.py
@@ -1,136 +1,71 @@
-############### Blackjack Project #####################
-
 import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-random_cards_pc = []
-random_card_selection_pc_1 = random.choice(cards)
-random_card_selection_pc_2 = random.choice(cards)
-random_cards_pc.append(random_card_selection_pc_1)
-random_cards_pc.append(random_card_selection_pc_2)
-sum_of_cards_pc = 0
-print(random_card_selection_pc_2)
+userName = input("Welcome to BlackJack game, enter your name: ")
+def selectCards():
+    return [random.choice(cards),random.choice(cards)]
 
-random_cards = []
-random_card_selection_1 = random.choice(cards)
-random_card_selection_2 = random.choice(cards)
-random_cards.append(random_card_selection_1)
-random_cards.append(random_card_selection_2)
-sum_of_cards = 0
-
-sum_of_cards = random_card_selection_1 + random_card_selection_2
-
-if random_card_selection_1 + random_card_selection_2 == 21:
-    print("Safak, BlackJack, you won.")
-
-print(random_cards)
-
-if sum_of_cards == 21 and sum_of_cards_pc == 21:
-    print("Draw, Double blackjack!!!")
+def isBlackJack(userHandTotal,PChandTotal):
+    if userHandTotal == 21:
+        print(f"{userName}, blackjack, you won!")
+    elif PChandTotal == 21:
+        print(f"PC, blackjack, you won!")
     
-more_card = "no"
-    
-if sum_of_cards > 21:
-    for i in range(len(random_cards)):
-        if random_cards[i] == cards[0]:
-            if sum_of_cards >= 22:
-                random_cards[i] = random_cards[i] - 10
-                sum_of_cards = sum_of_cards - 10
-                print(random_cards[i])
-                print(sum_of_cards)
+def isBusted(userHandTotal,PChandTotal):
+    if userHandTotal > 21 or PChandTotal > 21:
+        return True
+def elevenToOne(hand):
+    if sum(hand) >= 21 and 11 in hand:
+        hand[hand.index(11)] = 1
 
-if sum_of_cards < 21:
-    more_card = input("Do you want card? (yes/no): ")
+def PC_cardDecision(userHandTotal, PChandTotal):
+    if sum(PChandTotal) <= 21 and sum(PChandTotal)< sum(userHandTotal):
+        return True
+    if sum(userHandTotal) <= 10:
+        return True
+    return False
+    
+def BlackJack():
+    userHand = []
+    PCHand = []
+    userHand = selectCards()
+    PCHand = selectCards()
+    print(userHand)
+    print(PCHand[1])
 
-while more_card == "yes":
-    random_card_selection_more = random.choice(cards)
-    random_cards.append(random_card_selection_more)
-    print(f"random cards{random_cards}")
-    sum_of_cards += random_card_selection_more
-    
-    for i in range(len(random_cards)):
-        if random_cards[i] == cards[0]:
-            if sum_of_cards >= 22:
-                random_cards[i] = random_cards[i] - 10
-                sum_of_cards = sum_of_cards - 10
-                print(random_cards[i])
-                print(sum_of_cards)
-            elif sum_of_cards >= 22:
-                more_card = "no"
-        
-    if sum_of_cards == 21:
-        print("Safak, BlackJack, you won.")
-        more_card_pc = "no"
-        break
-    if sum_of_cards < 21:
-        more_card = input("Do you want card? (yes/no): ")
-    
-    if more_card == "no":
-        more_card = "no"
-        print(f"Safak: {random_cards}")
-
-sum_of_cards_pc = random_card_selection_pc_1 + random_card_selection_pc_2
-
-more_card_pc = "no"
-if sum_of_cards_pc < sum_of_cards:
-    if sum_of_cards_pc < 22:
-        if sum_of_cards < 22:
-            more_card_pc = "yes"
-            
-        elif sum_of_cards == 21:
-            more_card_pc = "no"
-    
-elif sum_of_cards == sum_of_cards_pc:
-    print("Draww eşit")
-    
-while more_card_pc == "yes":
-    if random_card_selection_pc_1 + random_card_selection_pc_2 == 21:
-        print("PC, BlackJack, PC won.")
-        break
-    
-    if sum_of_cards < sum_of_cards_pc:    
-        if sum_of_cards_pc < 22:
-            print("PC won!")
+    moreCard = "yes"
+    while moreCard == "yes":
+        moreCard = input("Do you want card?: ").lower()
+        if moreCard == "yes":    
+            userHand.append(random.choice(cards))
+            elevenToOne(userHand)
+            print(userHand)
+            print(PCHand[1])
+        else:
             break
+            
+    while PC_cardDecision(userHand,PCHand):
+        PCHand.append(random.choice(cards))
+        elevenToOne(PCHand)
+            
+    print(userHand)
+    print(PCHand)
         
-    if sum_of_cards_pc > 21:
-        more_card_pc = "no"
-        break
+    result = isBlackJack(userHand, PCHand)
+    if result is True:
+        print(result)
+    elif sum(userHand) > 21:
+        print(f"{userName} busted, PC won!")
+    elif sum(PCHand) > 21:
+        print(f"PC busted, Safak Won!")
+    elif sum(userHand) > sum(PCHand):
+        print(f"{userName} Won!")
+    elif sum(PCHand) > sum(userHand):
+        print(f"PC Won!")
+    elif sum(PCHand) == sum(userHand):
+        print(f"Draw, it is a tie.")
+    else:
+        print("PC won!")
     
-    random_card_selection_pc_more = random.choice(cards)
-    random_cards_pc.append(random_card_selection_pc_more)
-    sum_of_cards_pc += random_card_selection_pc_more
-    
-    for i in range(len(random_cards_pc)):
-        if random_cards_pc[i] == cards[0]:
-            if sum_of_cards_pc >= 22:
-                random_cards_pc[i] = random_cards_pc[i] - 10
-                sum_of_cards_pc = sum_of_cards_pc - 10
-
-    if sum_of_cards_pc >= 22:
-        print("PC lost this hand")
-        break
-    
-    if sum_of_cards_pc == 21:
-        print("PC, BlackJack, PC won.")
-        break
-    
-    if random_card_selection_pc_1 + random_card_selection_pc_2 <= random_card_selection_1 + random_card_selection_2:
-        if random_card_selection_pc_1 + random_card_selection_pc_2 > 21:
-            more_card_pc = "no"
-        elif random_card_selection_pc_1 + random_card_selection_pc_2 < 21:
-            more_card_pc = "yes"
-    
-    elif random_card_selection_pc_1 + random_card_selection_pc_2 <= random_card_selection_1 + random_card_selection_2:
-        more_card_pc = "no"
-        
-if sum_of_cards > sum_of_cards_pc:
-    if sum_of_cards <= 21:
-        print("Safak you won!")
-        
-if sum_of_cards >= 21:
-        print("Safak busted!, you lost!")
-        more_card = "no"
-        
-print(f"PC: {random_cards_pc}")
+BlackJack()
