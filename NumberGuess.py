1327054977fe88e42b57cf3e3812400714d22eab
Şafak Yılmaz <68486747+safakyilmaz-mis@users.noreply.github.com>
2023-11-20 23:23:52 +0100

    Update NumberGuess.py
    bug fixes and made optimisations

--- NumberGuess.py
+++ NumberGuess.py
@@ -1,63 +1,46 @@
 import random
 
+MODE = 0
 def guess_number():
     number = random.randint(1, 100)
     return number
 
-def mode(mode):
-    if mode == "easy":
-        mode == "easy"
-        print("You have 10 attemps remaining to guess the number.")
+def mode(MODE):
+    if MODE == "easy":
+        return f"You have {life_counter(MODE)+1} attemps remaining to guess the number."
         
-    elif mode == "hard":
-        mode == "hard"
-        print("You have 5 attemps remaining to guess the number.")
+    elif MODE == "hard":
+        return f"You have {life_counter(MODE)+1} attemps remaining to guess the number."
         
     else:
-        print("wrong answer for mode selection please restart again")
-        
-        
-def attempt(mode):
-    if mode == "easy":
-        mode == "easy"
-        print("You have 10 attemps remaining to guess the number.")
-        
-    elif mode == "hard":
-        mode == "hard"
-        print("You have 5 attemps remaining to guess the number.")
-        
-    else:
-        print("wrong answer for mode selection please restart again")
-        
-def life_counter(mode):
-    if mode == "hard":
+        return "wrong answer for mode selection please restart again"
+                
+def life_counter(MODE):
+    if MODE == "hard":
         return 4
-    elif mode == "easy":
+    elif MODE == "easy":
         return 9
-    
 
 def main():
     Guess_number = guess_number()
     print("Welcome to the Number Guessing game!")
     print("I'm thinking of a number between 1 and 100")
-    mode = input("Choose difficulty. Type 'easy' or 'hard': ") 
+    MODE = input("Choose difficulty. Type 'easy' or 'hard': ") 
+    print(mode(MODE))
     guess = int(input("Make a guess: "))
-    life = life_counter(mode)
+    life = life_counter(MODE)
     while(guess != Guess_number):  
         if life == 0:
             print(f"Game over you finished all of your attemps.\nCorrect answer was '{Guess_number}'")
             break
-        
         if guess < Guess_number:
             print("Too low. \n Guess again.")
             life -= 1
             guess = int(input("Guess again: "))
-            
         if guess > Guess_number:
             print("Too high. \n Guess again.")
             life -= 1
             guess = int(input("Guess again: "))
-            
     if guess == Guess_number:
         print("Perfect, you guessed correct.")
         
