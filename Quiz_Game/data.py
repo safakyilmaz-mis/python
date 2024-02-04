3b371fd0d77a672b34872d0c9dd967be2d240049
Şafak Yılmaz <68486747+safakyilmaz-mis@users.noreply.github.com>
2024-02-04 18:06:18 +0100

    Create data.py

---
+++ Quiz_Game/data.py
@@ -0,0 +1,37 @@
+#you can change the datas from https://opentdb.com/api_config.php adress
+
+question_data = [
+    {"type": "boolean",
+     "difficulty": "medium",
+     "category": "Entertainment: Television",
+     "question": "In &quot;Star Trek: The Next Generation&quot;, Data is the only android in existence.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "hard", "category": "Entertainment: Video Games",
+     "question": "The retail disc of Tony Hawk&#039;s Pro Skater 5 only comes with the tutorial.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Video Games",
+     "question": "In &quot;Super Mario 64&quot;, collecting 100 coins on a level will give you a 1-UP.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Music",
+     "question": "In 1993, Prince changed his name to an unpronounceable symbol because he was unhappy with his "
+                 "contract with Warner Bros.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "hard", "category": "Science: Mathematics",
+     "question": "In Topology, the complement of an open set is a closed set.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "medium", "category": "Entertainment: Cartoon &amp; Animations",
+     "question": "Nutcracker Suite was one of the musical pieces featured in Disney&#039;s 1940&#039;s film Fantasia.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+     "question": "Fast food restaurant chains Carl&#039;s Jr. and Hardee&#039;s are owned by the same company.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Books",
+     "question": "Stephen Chbosky wrote the book &#039;The Perks of Being A Wallflower&#039;.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "hard", "category": "General Knowledge",
+     "question": "&quot;Number 16 Bus Shelter&quot; was a child&#039;s name that was approved by the New Zealand "
+                 "government.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Geography", "question": "California is larger than Japan.",
+     "correct_answer": "True", "incorrect_answers": ["False"]}]
