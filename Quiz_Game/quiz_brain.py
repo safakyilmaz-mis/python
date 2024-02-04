5caf0ace7f8b86da0e48e197776f7399ec72748c
Şafak Yılmaz <68486747+safakyilmaz-mis@users.noreply.github.com>
2024-02-04 18:07:14 +0100

    Create quiz_brain.py

---
+++ Quiz_Game/quiz_brain.py
@@ -0,0 +1,29 @@
+class QuizBrain:
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+        self.score = 0
+
+    def still_has_question(self):
+        return self.question_number < len(self.question_list)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You are right!")
+            self.score += 1
+        else:
+            print("That was wrong!")
+        print(f"Correct answer was: {correct_answer}")
+        print(f"Your current score is: {self.score}/{len(self.question_list)}")
+        print("\n")
+
+    def total_score(self):
+        question_list = len(self.question_list)
+        score = self.score
+        return f"{score}/{question_list}"
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+        self.check_answer(user_answer, current_question.answer)
