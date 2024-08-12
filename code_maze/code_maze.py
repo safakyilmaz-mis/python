883674827ed96953027654d893c68ee825f3d006
Şafak Yılmaz <68486747+safakyilmaz-mis@users.noreply.github.com>
2024-08-12 15:09:16 +0200

    Update code_maze.py

--- code_maze/code_maze.py
+++ code_maze/code_maze.py
@@ -4,27 +4,23 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 import time
-import json
 import pymongo
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
-#Start WebDriver
+# Start WebDriver
 driver = webdriver.Chrome(options=chrome_options)
 
-#Go to site
+# Go to site
 driver.get("https://code-maze.com/latest-posts-on-code-maze/?et_blog")
 
-#Lists
-links = []
-photos = []
-titles = []
-shrt_des = []
+# Initialize list to store scraped data
+scraped_data = []
 
-#Go to next page and parse datas
+# Go to next page and parse data
 while True:
-    #Reject cookies
+    # Reject cookies
     time.sleep(5)
     try:
         cookie_response = driver.find_element(By.XPATH, '//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]')
@@ -32,47 +28,55 @@ while True:
     except NoSuchElementException:
         pass
 
-    #Get page's source and parse with bs4
+    # Get page's source and parse with bs4
     response = bs4(driver.page_source, "html.parser")
 
-    #Links
-    for a in response.find_all('a', class_="entry-featured-image-url", href=True):
-        links.append(a['href'])
+    # Find all articles on the page
+    articles = response.find_all('article')
 
-    #Photos
-    image_tags = response.find_all("img")
-    for image_tag in image_tags:
-        img_src = image_tag.get("src")
-        if img_src.startswith("data:image/svg+xml") or img_src == "https://code-maze.com/wp-content/uploads/2021/02/Code-Maze-Logo-White-Text-Transparent-Small.png":
-            continue
-        else:
-            photos.append(img_src)
+    for article in articles:
+        # Get the link
+        link_tag = article.find('a', class_="entry-featured-image-url", href=True)
+        link = link_tag['href'] if link_tag else None
 
-    #Titles
-    for a in response.find_all('h2', class_="entry-title"):
-        titles.append(a.text)
+        # Get the photo
+        img_tag = article.find("img")
+        img_src = img_tag.get("src") if img_tag else None
+        if img_src and (img_src.startswith("data:image/svg+xml") or img_src == "https://code-maze.com/wp-content/uploads/2021/02/Code-Maze-Logo-White-Text-Transparent-Small.png"):
+            img_src = None  # Exclude unwanted images
 
-    #Short Descriptions
-    for a in response.find_all('div', class_="post-content-inner"):
-        shrt_des.append(a.text)
+        # Get the title
+        title_tag = article.find('h2', class_="entry-title")
+        title = title_tag.text if title_tag else None
 
-    #Next page
+        # Get the short description
+        shrt_des_tag = article.find('div', class_="post-content-inner")
+        shrt_des = shrt_des_tag.text if shrt_des_tag else None
+
+        # Store the data in a dictionary and append to list
+        if title and link:  # Ensure required fields are present
+            scraped_data.append({
+                "title": title,
+                "photo": img_src,
+                "short_description": shrt_des,
+                "link": link
+            })
+
+    # Next page
     try:
         older_post = driver.find_element(By.XPATH, '//*[@id="et-boc"]/div/div/div/div/div/div[1]/div/div/div[2]/nav/div/div[1]/a/span')
         older_post.click()
     except NoSuchElementException:
         break
 
-#write datas to file which collected
-dict_maze = {titles[i]: [photos[i], shrt_des[i], links[i]] for i in range(len(links))}
-with open('dictionary_maze.json', 'a', encoding='utf-8') as f:
-    json.dump(dict_maze, f, ensure_ascii=False, indent=4)
+# MongoDB connection
+client = pymongo.MongoClient('mongodb+srv://safakyilmaz:yAmLCoUPLrWM1LCe@denememaze.eb4jt.mongodb.net/')
+db = client['code_maze_all_pages']  # Replace with your database name
+collection = db['contents']  # Replace with your collection name
 
-
-client = pymongo.MongoClient('mongodb://username:password@host:port/?directConnection=true')
-db = client.db.quotes
+# Insert scraped data into MongoDB
 try:
-    db.insert_many(dict_maze)
-    print(f'inserted {len(dict_maze)} articles')
-except:
-    print('an error occurred quotes were not stored to db')
+    result = collection.insert_many(scraped_data)
+    print(f'Inserted {len(result.inserted_ids)} articles into MongoDB.')
+except Exception as e:
+    print(f'An error occurred: {e}')
