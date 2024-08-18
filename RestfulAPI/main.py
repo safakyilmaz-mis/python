5633850c164d4bd24f7d1bb7027aca1f606191a2
Şafak Yılmaz <68486747+safakyilmaz-mis@users.noreply.github.com>
2024-08-18 19:42:27 +0200

    RESTAPI first project
    Cafe api

--- RestfulAPI/main.py
+++ RestfulAPI/main.py
@@ -1,9 +1,10 @@
 import random
 from random import choice
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, json, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
+from sqlalchemy.sql import text
 
 '''
 Install the required packages first: 
@@ -51,25 +52,78 @@ with app.app_context():
 @app.route("/")
 def home():
     return render_template("index.html")
-    
-
-
-# HTTP GET - Read Record
-# @app.route("/random", methods=["GET"])
-# def random_cafe():
-#     pass
-
-@app.route("/random", methods=["GET"])
-def random_cafe():
+ 
+@app.route("/random")
+def random():
     result = db.session.execute(db.select(Cafe))
     all_cafes = result.scalars().all()
-    random_cafes = choice(all_cafes)
+    random_cafe = choice(all_cafes)
+    random_cafe_select = {key: value for key,value in random_cafe.__dict__.items() if key != '_sa_instance_state'}
+    return jsonify(random_cafe=random_cafe_select)
 
+
+@app.route("/all")
+def all():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    cafes_list = [{column.name: getattr(cafe, column.name) for column in Cafe.__table__.columns} for cafe in all_cafes]
+    return jsonify(All_cafes=cafes_list)
+
+@app.route("/search")
+def search():
+    location = request.args.get('loc')
+    result = db.session.execute(db.select(Cafe).where(Cafe.location == location)).scalars().all()
+    if result:
+        return jsonify(Filtered_cafes = [{column.name: getattr(cafe, column.name) for column in Cafe.__table__.columns} for cafe in result])
+    else:
+        return jsonify(Error="No cafes found for the given location."),404
+
+    
 # HTTP POST - Create Record
+
+@app.route("/add", methods=["GET","POST"])
+def add():
+    new_cafe = Cafe(name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),)
+
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(message="Cafe added successfully."), 201    
 
 # HTTP PUT/PATCH - Update Record
 
+from werkzeug.exceptions import HTTPException
+
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+def update_cafe_price(cafe_id):
+    new_price = request.args.get("new_price")
+    cafe = db.session.execute(db.select(Cafe).where(Cafe.id == cafe_id)).scalar()
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={f"success": f"Successfully update price for the cafe {cafe.name}."})
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
+
 # HTTP DELETE - Delete Record
+
+@app.route("/delete/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    cafe = db.session.execute(db.select(Cafe).where(Cafe.id == cafe_id)).scalar()
+    if cafe:
+        db.session.delete(cafe)
+        db.session.commit()
+        return jsonify(message=f"Successfully deleted cafe {cafe.name}.")
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
 
 
 if __name__ == '__main__':
