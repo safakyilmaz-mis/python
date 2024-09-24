02af2d6bda720926cf595bfe3dae369ef8d0985e
Şafak Yılmaz <safakyilmaz@yandex.com>
2024-09-24 16:43:19 +0200

    fast api project

--- Fast_api.py
+++ Fast_api.py
@@ -3,6 +3,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+def root():
+    return {"Helloo": "World"}
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/names/{name}")
+def read_name(name: str):
+    return {"name": name}  # Eksik olan return ifadesi tamamlandı
