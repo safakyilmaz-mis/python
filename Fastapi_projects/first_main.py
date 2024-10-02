664a59764ecbc9be5756f4f94cf981c8c81539c4
Şafak Yılmaz <safakyilmaz@yandex.com>
2024-10-02 23:27:05 +0200

    Fast api course update

--- Fastapi_projects/first_main.py
+++ Fastapi_projects/first_main.py
@@ -1,7 +1,13 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Users(BaseModel):
+    name: str
+    age: int
+    YOB: int
 
 @app.get("/")
 async def root():
@@ -11,3 +17,10 @@ async def root():
 async def about_us(aaa, name: Optional[str] = "User", age: Optional[int] = 0) -> dict:
     return {"message":f"{aaa}, Hello {name}, you are {age} years old"}
 
+@app.post("/users")
+async def Create_user(create_user: Users):
+    return{
+        "name": create_user.name,
+        "age": create_user.age,
+        "YOB": create_user.YOB
+    }
