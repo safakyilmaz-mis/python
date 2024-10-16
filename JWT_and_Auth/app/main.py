9c2d70754c72b67c6a3c42d38894fcb021f65e61
Şafak Yılmaz <safakyilmaz@yandex.com>
2024-10-16 15:03:21 +0200

    token time extended

--- JWT_and_Auth/app/main.py
+++ JWT_and_Auth/app/main.py
@@ -16,7 +16,7 @@ from pydantic import BaseModel, ValidationError
 # openssl rand -hex 32
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 
 fake_users_db = {
