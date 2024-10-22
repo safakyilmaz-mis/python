c53dc01e92b6063f955e0f0103ceaeeaba3a3db4
Şafak Yılmaz <safakyilmaz@yandex.com>
2024-10-22 10:19:16 +0200

    added comments and unnecessary codes changed with better functions
    added comments and unnecessary codes changed with better functions

--- Linkedin_WS/linkedin_WS.py
+++ Linkedin_WS/linkedin_WS.py
@@ -1,6 +1,8 @@
 import time
 from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 proxy = '94.74.159.94:59100'
 
@@ -12,24 +14,27 @@ proxy_options = {
     }
 }
 
-driver = webdriver.Chrome(seleniumwire_options=proxy_options)
+# Add options to keep the browser open
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(seleniumwire_options=proxy_options, options=chrome_options)
 
 driver.get("https://www.linkedin.com/search/results/CONTENT/")
 
-time.sleep(5)
+# Wait for the email field to be visible
+email = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "username"))
+)
+email.send_keys('sarduc891@gmail.com')
 
-email = driver.find_element(By.XPATH, '//*[@id="username"]')
-email.click()
+password = driver.find_element(By.ID, 'password')
+password.send_keys('12501250')
 
-email.send_keys('xxxxxx@gmail.com')
-
-password = driver.find_element(By.XPATH, '//*[@id="password"]')
-password.click()
-password.send_keys('xxxxxx')
-
-login = driver.find_element(By.XPATH, '//*[@id="organic-div"]/form/div[4]/button')
+login = driver.find_element(By.XPATH, '//button[@type="submit"]')
 login.click()
 
+# Keep the script running
 input("Press Enter to close the browser...")
 
 # The browser will only close after you press Enter in the console
@@ -37,4 +42,3 @@ driver.quit()
 
 # search_button = driver.find_element(By.XPATH, '//*[@id="global-nav-typeahead"]/input')
 # search_button.click()
-
