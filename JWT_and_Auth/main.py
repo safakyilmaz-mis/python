e2a2b472b481af7defac2053910df1b11546337f
Şafak Yılmaz <safakyilmaz@yandex.com>
2024-11-06 22:44:22 +0100

    understand the Oauth2 and JWT project

--- JWT_and_Auth/main.py
+++ JWT_and_Auth/main.py
@@ -4,15 +4,15 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI()
 
-Oauth2 = OAuth2PasswordBearer(tokenUrl = "token")
+Oauth2 = OAuth2PasswordBearer(tokenUrl = "vip-token")
 
 # once post ile token yolluyoruz sisteme ardindan korunan ana sayfaya gidiyoruz 
 # cunku once erisim almamiz gerekli izin almamiz gerekli token linkinden
 
 @app.post("/vip-token")
-async def vip_token(vip_data: OAuth2PasswordRequestForm = Depends()):
-    return {"access_token": vip_data.username+"6969"}
+async def vip_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    return {"access_token": form_data.username+"6969"}
 
 @app.get("/vip-section")
 async def vip_section(token: str= Depends(Oauth2)):
-    return {"the_token": token}
+    return {"message":"welcome to vip section","the_token": token}
